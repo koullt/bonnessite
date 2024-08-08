1612c2fc6eb5d76688610b76e423ecc912fea383
koullt <contato.cmazi@gmail.com>
2024-08-08 17:26:00 -0300

    Deploy website Bonnes

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function changebg(bg, title) {
+    const banner = document.querySelector('.banner');
+    const contents = document.querySelectorAll('.content');
+    banner.style.backgroundImage = `url("./images/index/${bg}")`;
+    banner.style.backgroundSize = 'cover';
+    banner.style.backgroundPosition = 'center';
+    
+    contents.forEach(content => {
+        content.classList.remove('active');
+        if (content.classList.contains(title)) {
+            content.classList.add('active');
+        }
+    });
+}
