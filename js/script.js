07091375fb8a38dcb638280fc21fa7c7d5e874d3
koullt <contato.cmazi@gmail.com>
2024-08-09 17:02:29 -0300

    atualização index

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ function changebg(bg, title) {
         }
     });
 }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
